2ee369ce810cdab950b767d0615eba07bcd1a36e
Jesus <martoscj@gmail.com>
2020-07-25 12:23:39 +0200

    Added parameterized test.

--- test/electrons/test_parabolic.py
+++ test/electrons/test_parabolic.py
@@ -1,15 +1,22 @@
 import numpy as np
+import pytest
 
 from hektor.electrons.parabolic import getbetar
 
 
-def test_getbetar():
-    h = np.array([3, 3.5])
-    r = np.array([0.52, 0.55])
-    Jr = np.array([1.2, 1.1])
-    ptheta = np.array([0.1, 0.11])
-
-    expected_betar = np.array([0.058975519032537, 0.049668031380904])
+@pytest.mark.parametrize(
+    "h, r, Jr, ptheta, expected_betar",
+    [
+        [
+            np.array([3, 3.5]),
+            np.array([0.52, 0.55]),
+            np.array([1.2, 1.1]),
+            np.array([0.1, 0.11]),
+            np.array([0.058975519032537, 0.049668031380904]),
+        ]
+    ],
+)
+def test_getbetar(h, r, Jr, ptheta, expected_betar):
 
     betar = getbetar(h, r, Jr, ptheta)
     np.testing.assert_almost_equal(betar, expected_betar)
