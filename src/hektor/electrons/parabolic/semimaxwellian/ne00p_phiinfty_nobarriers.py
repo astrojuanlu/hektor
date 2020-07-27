9c192a8119e405afa164954a551501321fe7ee67
asaez <alex.saez.12@gmail.com>
2020-07-27 11:24:01 +0200

    use Newton method to instead of root_scalar

--- src/hektor/electrons/parabolic/semimaxwellian/ne00p_phiinfty_nobarriers.py
+++ src/hektor/electrons/parabolic/semimaxwellian/ne00p_phiinfty_nobarriers.py
@@ -1,15 +1,17 @@
 import numpy as np
 from scipy.special import erf
-from scipy.optimize import root_scalar
+from scipy.optimize import newton
 
 
 def ne00p_phiinfty_nobarriers(chi):
     # MATLAB fzero(@(phiinfty) eq1(phiinfty)-eq2(phiinfty),-5);
-    # Ideal Python: root_scalar(eq1_minus_eq2, args=(chi,), x0=-5)
-    sol = root_scalar(eq1_minus_eq2, args=(chi,), x0=-5)
-    phiinfty = sol.root
+    # Ideal Python: root_scalar(eq1_minus_eq2, args=(chi,), x0=-5) does not pick Newton
+    # sol = root_scalar(eq1_minus_eq2, args=(chi,), x0=-5)
+    # phiinfty = sol.root
+    # Select Newton method manually -> does not converge with this initial condition
+    # ie. x0=-2 does converge for the current tests
+    phiinfty = newton(eq1_minus_eq2, args=(chi,), x0=-5)
     ne00p = 1 / eq2(phiinfty, chi)
-
     return ne00p, phiinfty
 
 
